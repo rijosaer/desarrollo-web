2fd5c0791080ee95143dc79843be33094f8b6a9f
rijosaer <rboyaca@uniguajira.edu.co>
2025-09-03 12:13:25 -0500

    ver y crear autor pendiente

--- library_2.0/src/app/components/author/show-author/show-author.ts
+++ library_2.0/src/app/components/author/show-author/show-author.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { AuthorI } from '../../../models/author';
+import { ButtonModule } from 'primeng/button';
+
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-show-author',
-  imports: [],
+  standalone: true,
+  imports: [TableModule, CommonModule, ButtonModule, RouterModule],
   templateUrl: './show-author.html',
   styleUrl: './show-author.css'
 })
-export class ShowAuthor {
-
+export class ShowAuthor  {
+ author: AuthorI[] = [
+  {
+    id: 1,
+    name: "Ricardo Boyaca",
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: "Johan Boyaca",
+    status: 'active',
+  }
+ ];
+ 
 }
---
+++ library_2.0/src/app/models/author.ts
@@ -0,0 +1,5 @@
+export interface AuthorI {
+            id?: number;
+            name: string;
+            status: 'active' | 'inactive'
+}
